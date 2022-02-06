045875d96d31652a45c68083d67d76b5f11b234f
emsiBt <tranthanhbao0309@gmail.com>
2022-02-06 11:42:09 +0700

    Completed

---
+++ base/urls.py
@@ -0,0 +1,21 @@
+from unicodedata import name
+from django.urls import path
+from .views import RegisterUser, TaskCreate, TaskDelete, TaskList, TaskDetail, TaskUpdate, CustomLogin
+from django.contrib.auth.views import LogoutView
+
+urlpatterns =[
+    path('login', CustomLogin.as_view(), name='login'),
+
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('register', RegisterUser.as_view(), name='register'),
+    path('', TaskList.as_view(), name='tasks'),
+
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+]
